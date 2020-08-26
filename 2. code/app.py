dbb5668146779671c8bc0a947727ba90df443070
song1413 <song14132@gmail.com>
2020-08-27 08:26:06 +0900

    update 2020.08.27

--- 2. code/app.py
+++ 2. code/app.py
@@ -27,6 +27,43 @@ def bookAdd2():
     return render_template(
         'bookAdd2.html',isbn=temp)
 
+## API 역할을 하는 부분
+@app.route('/booksave', methods=['POST'])
+def addBook():
+	# 1. 클라이언트가 준 title, author, review 가져오기.
+    thumbnail_receive = request.form['thumbnail_give']
+    title_receive = request.form['title_give']
+    author_receive = request.form['author_give']
+    publisher_receive = request.form['publisher_give']
+    datetime_receive = request.form['datetime_give']
+    isbn_receive = request.form['isbn_give']
+
+	# 2. DB에 정보 삽입하기
+    book = {
+        'thumbnail': thumbnail_receive,
+        'title': title_receive,
+        'author': author_receive,
+        'publisher': publisher_receive,
+        'datetime': datetime_receive,
+        'isbn': isbn_receive
+    }
+
+    # 3. books에 book 저장하기
+    db.books.insert_one(book)
+
+	# 4. 성공 여부 & 성공 메시지 반환하기
+    return jsonify({'result': 'success', 'msg': '책장에 등록되었습니다'})
+
+@app.route('/booksave', methods=['GET'])
+def read_books():
+    return jsonify({'result': 'success', 'msg': '이 요청은 GET!'})
+
+
+@app.route('/bookadd4')
+def bookAdd4():
+    return render_template('bookAdd4.html', )
+
+
 
 # API 역할을 하는 부분
 @app.route('/api/list', methods=['GET'])
